0ff119be75f66c7f47fd2068d2a213b514602aed
Rick <rickoliwer@gmail.com>
2021-10-14 10:03:10 +0200

    rest api/CRUD

---
+++ pages/api/createMenu.js
@@ -0,0 +1,18 @@
+import {table, minifyRecords} from './utils/Airtable'
+export default async (req, res) => {
+    const {day, first_dish, second_dish} = req.body;
+    
+    try {
+
+        const createdRecords = await table.create([{fields: {day, first_dish, second_dish}}]);
+        const createdRecord = {
+            id: createdRecords[0].id,
+            fields: createdRecords[0].fields
+        }
+        
+        res.status(200).json(createdRecord);
+    }catch(err){
+        res.status(500).json({ msg: 'Something went wrong'});
+    }
+}
+  
---
+++ pages/api/getMenu.js
@@ -0,0 +1,14 @@
+import {table, minifyRecords} from './utils/Airtable'
+export default async (req, res) => {
+    try {
+
+        const records = await table.select({
+            view: "Grid view"
+        }).firstPage();
+        const minifiedRecords = minifyRecords(records);
+        res.status(200).json(minifiedRecords);
+    }catch(err){
+        res.status(500).json({ msg: 'Something went wrong'});
+    }
+}
+  
